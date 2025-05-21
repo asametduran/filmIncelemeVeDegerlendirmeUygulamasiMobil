f2b206179cae6a525c45e7c5292510d74b138f0a
asametduran <asametdurann@gmail.com>
2025-05-21 23:01:15 +0300

    TMDB API için hazırlıklar yapıldı

---
+++ services/api.ts
@@ -0,0 +1,67 @@
+export const TMDB_CONFIG = {
+    BASE_URL: "https://api.themoviedb.org/3",
+    API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+    headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+    },
+};
+
+export const fetchMovies = async ({
+                                      query,
+                                  }: {
+    query: string;
+}): Promise<Movie[]> => {
+    const endpoint = query
+        ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+    const response = await fetch(endpoint, {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch movies: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.results;
+};
+
+export const fetchMovieDetails = async (
+    movieId: string
+): Promise<MovieDetails> => {
+    try {
+        const response = await fetch(
+            `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+            {
+                method: "GET",
+                headers: TMDB_CONFIG.headers,
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch movie details: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching movie details:", error);
+        throw error;
+    }
+};
+// const url = 'https://api.themoviedb.org/3/keyword/keyword_id/movies?include_adult=false&language=en-US&page=1';
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         accept: 'application/json',
+//         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZmI2ZDIxNDE0NTE2MWQ3NDEzMTlmMzQwZDdlODk2MCIsIm5iZiI6MTcyNjM5MjcxMS4yNTUsInN1YiI6IjY2ZTZhOTg3OWRmYmJkZjBlNmNmNjdhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.b1Xlo2Y_wyqlCTMDPItMHO6raUoE_dExg3N3qv3IPWA'
+//     }
+// };
+//
+// fetch(url, options)
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+//     .catch(err => console.error(err));
